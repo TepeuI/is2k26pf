c62a145c17801a39df5db146c09fdf979b53ef07
RichardAnthonyDeLeon <richardmilian10@gmail.com>
2026-05-02 22:44:13 -0600

    prueba1

--- codigo/empresarial/Equipo 2/DISTRIBUCION/Proceso4-Comprobantes-Richard-de-Leon/Capa_Vista_Comprobantes/Frm_Comprobante_Venta.Designer.cs
+++ codigo/empresarial/Equipo 2/DISTRIBUCION/Proceso4-Comprobantes-Richard-de-Leon/Capa_Vista_Comprobantes/Frm_Comprobante_Venta.Designer.cs
@@ -95,7 +95,7 @@ namespace Capa_Vista_Comprobantes
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(675, 79);
+            this.textBox2.Location = new System.Drawing.Point(657, 46);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(193, 26);
             this.textBox2.TabIndex = 54;
@@ -165,7 +165,7 @@ namespace Capa_Vista_Comprobantes
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(720, 53);
+            this.label8.Location = new System.Drawing.Point(702, 20);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(96, 14);
             this.label8.TabIndex = 47;
