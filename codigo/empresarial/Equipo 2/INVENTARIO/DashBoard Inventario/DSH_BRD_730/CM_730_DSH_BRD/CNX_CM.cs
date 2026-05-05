00294c8132ab5fd68833b9be10fbea3b3e970b9e
Ferjo-Alz <fejomial@gmail.com>
2026-05-05 10:12:17 -0600

    DashBoard 80 Inventario

---
+++ codigo/empresarial/Equipo 2/INVENTARIO/DashBoard Inventario/DSH_BRD_730/CM_730_DSH_BRD/CNX_CM.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Odbc;
+
+public class CNX
+{
+    private static string cadena = "DSN=bd_SIG;Uid=root;Pwd=Acces0F3R;";
+
+    public static OdbcConnection ObtenerConexion()
+    {
+        return new OdbcConnection(cadena);
+    }
+}
